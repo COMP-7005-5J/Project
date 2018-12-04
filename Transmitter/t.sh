44a6aa4cce47e80fcb4aa2647ff47ece4d9f2bc1
Davin Deol <davindeol@gmail.com>
2018-12-03 19:51:10 -0800

    Added test document

--- Transmitter/t.sh
+++ Transmitter/t.sh
@@ -2,4 +2,6 @@ clear
 gcc -c -Wall -o ../l ../Log.c 
 gcc -c -Wall -o tra Transmitter.c
 gcc ../l tra -o tr
-{ echo "localhost"; echo "8003"; echo "1.txt"; } | ./tr
+./tr
+
+#{ echo "localhost"; echo "8003"; echo "1.txt"; } | ./tr
